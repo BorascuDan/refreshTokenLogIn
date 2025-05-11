f4461d9a755e75be72193249f89e5b673ff176ee
dan <borascu.dan24@gmail.com>
2025-05-12 00:58:15 +0300

    init commit

---
+++ index.mjs
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from 'dotenv'
+import apiRouter from './src/routes/apiRoute.mjs'
+import db from "./src/utils/database.mjs";
+
+const app = express();
+
+
+dotenv.config()
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/', apiRouter)
+
+app.get('/health', async (req, res) => {
+  let dbConnection;
+  try {
+    console.log('florea');
+    await db.raw('SELECT 1+1 AS result');
+    dbConnection = "OK";
+  } catch (error) {
+    console.log("ERROR: ", error)
+    dbConnection = false;
+  }
+
+  return res.status(200).json({
+    status: 'UP',
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+    db_connection: dbConnection
+  });
+})
+
+export default app;
